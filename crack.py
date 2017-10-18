3730456131117ca084572cfc347b947cac833d32
root <root@localhost.localdomain>
2017-10-18 14:26:22 +0100

    First script

---
+++ crack.py
@@ -0,0 +1,29 @@
+import crypt
+
+def testPass(cryptPass):
+    if cryptPass[0:3] == "$6$"
+        salt = cryptPass[0:9]
+    else
+        salt = cryptPass[0:2]
+    print "[+] Salt is:"+salt+"\n"
+    dictFile = open('dictionary.txt','r')
+    for word in dictFile.readlines():
+        word = word.strip('\n')
+        cryptWord = crypt.crypt(word,salt)
+        if (cryptWord == cryptPass):
+            print "[+] Found Password: "+word+"\n"
+            return
+        print "[-]Password Not Found.\n"
+        return
+
+def main():
+    passFile = open('password.txt')
+    for line in passFile.readlines():
+        if ":" in line:
+            user = line.split(':')[0]
+            cryptPass = line.split(':')[1].strip(' ')
+            print "[*] Cracking Password For: "+user+"\n"
+            testPass(cryptPass)
+
+if __name__ == "__main__":
+    main()
